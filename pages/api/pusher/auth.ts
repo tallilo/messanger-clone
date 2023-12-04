44ed3891a0129d0d752c9d8182ebc692c9c5fe2e
tallilo <tallilo5011323@gmail.com>
2023-12-04 20:03:53 +0200

    first commit

--- pages/api/pusher/auth.ts
+++ pages/api/pusher/auth.ts
@@ -1,8 +1,8 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import { getServerSession } from "next-auth";
+import { getServerSession } from "next-auth/next";
 
 import { pusherServer } from "@/app/libs/pusher";
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { authOptions } from "@/app/api/auth/[...nextauth]/options";
 
 export default async function handler(
   request: NextApiRequest,
